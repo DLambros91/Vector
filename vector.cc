c3cfa859c1eccdd0bc6f55cb1d2a3ece04727749
Demetrios <d.lambropoulos91@gmail.com>
2022-01-04 20:11:46 -0500

    Update vector.cc
    Added driver main.
    
    Added function to get item at specified index.

--- vector.cc
+++ vector.cc
@@ -1,3 +1,7 @@
+#include <iostream>
+
+template <typename T>
+
 class vector {
     public: 
         int size () {
@@ -11,9 +15,37 @@ class vector {
         bool isEmpty () {
             return numItems == 0;
         }
-        
+
+        T at (int index) 
+        {
+            if (numItems == 0) 
+            {
+                std::cout << "Vector is empty. Index out of range." << std::endl;
+                return -1;
+            } else if (index >= numItems)
+            {
+                std::cout << "Index out of range." << std::endl;
+                return -1;
+            }
+
+            return arr[index];
+        }       
+
     private:
         int numItems = 0;
         int maxCapacity = 16;
+        T arr[16];
         void resize (int newSize) { }
 };
+
+int main () {
+    vector<int> v;
+
+    std::cout << "The current size of the vector is: " << v.size() << std::endl;
+
+    std::cout << "The maximum capacity of the vector is: " << v.capacity() << std::endl;
+
+    v.at(0);
+
+    return 0;
+}
