163e1c8efb0c256d55fcd80d6673378416edeab9
Demetrios <d.lambropoulos91@gmail.com>
2022-01-04 20:35:49 -0500

    Update vector.cc
    Added function push.
    
    Added skeletons for remainder to complete.

--- vector.cc
+++ vector.cc
@@ -29,8 +29,40 @@ class vector {
             }
 
             return arr[index];
-        }       
+        }
 
+        void push (T item) 
+        {
+            if (numItems == maxCapacity) {
+                resize(maxCapacity*2);
+            }
+            arr[numItems++] = item;
+        }
+
+        void insert (int index, T item) {
+
+        }   
+
+        void prepend (T item) {
+
+        } 
+
+        T pop () {
+            
+        } 
+
+        void deleteIndex (int index) {
+
+        }
+
+        void remove (T item) {
+
+        }
+
+        int find (T item) {
+
+        }
+        
     private:
         int numItems = 0;
         int maxCapacity = 16;
@@ -46,6 +78,16 @@ int main () {
     std::cout << "The maximum capacity of the vector is: " << v.capacity() << std::endl;
 
     v.at(0);
+    
+    v.push(5);
+    v.push(4);
+    v.push(3);
+    
+    std::cout << "The current size of the vector is: " << v.size() << std::endl;
+
+    std::cout << "The maximum capacity of the vector is: " << v.capacity() << std::endl;
+    
+    std::cout << "Item at index 1 is: " << v.at(1) << std::endl;
 
     return 0;
 }
